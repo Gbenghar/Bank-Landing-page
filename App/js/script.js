d2e6c831f45076ca0f7216eb6fd4b4e75b589f32
Gbenga Olatunji <gbengaolatunji21@gmail.com>
2023-11-26 16:16:46 +0100

    Heeader section completed

--- App/js/script.js
+++ App/js/script.js
@@ -1,20 +1,29 @@
 
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body')
 const Header = document.querySelector('.header');
 const Overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 
 btnHamburger.addEventListener('click', function(){
    
-
     if (Header.classList.contains('open')){// Close Hamburger Menu
+        body.classList.remove('noscroll')
         Header.classList.remove('open')
-        Overlay.classList.add('fade-out')
-        Overlay.classList.remove('fade-in')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out')
+        })
+       
     }
     else {// Open Hamburger Menu
+        body.classList.add('noscroll')
         Header.classList.add('open');
-        Overlay.classList.remove('fade-out')
-        Overlay.classList.add('fade-in')
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in')
+        })
+       
     }
 });
